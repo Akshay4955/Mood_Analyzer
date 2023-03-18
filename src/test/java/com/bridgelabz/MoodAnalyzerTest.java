0dd4fd934ebe22b51398a062f21c2a5e12b3f2a2
Akshay4955 <akshayshedge11@gmail.com>
2023-03-18 16:41:25 +0530

    Added: Code to analyze mood for given message

---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Sad Mood");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenOtherThanSad_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
